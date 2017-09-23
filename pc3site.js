792d3f51b8a5536365eeba83146038c5847179fd
sHale <sally.hale@homeport.org.uk>
2017-09-23 16:04:12 +0100

    reservation script and more styling

--- pc3site.js
+++ pc3site.js
@@ -18,4 +18,32 @@ $(document).ready(function() {
     $("#reservationsClick").on("click", function() {
         $("#reservations").removeClass("hidden").hide().fadeIn(800).siblings().addClass('hidden');
     });
+
+    //reservations
+
+    $("#searchButton").on('click', function() {
+        var branch = $('#branchMenu').val();
+        var guestNum = $('#numoguests').val();
+        var arrDate = $('#chosenday').val();
+        var peopleString = guestNum <= 1 ? "person" : "people";
+        var newArrDate = arrDate.split('-');
+        var monthsArr = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+        var monthNum = parseInt(newArrDate[1]);
+        var finalRes = monthsArr[monthNum - 1];
+        var newDateWordMsg = newArrDate[2] + " " + finalRes + " " + newArrDate[0];
+
+        if (!guestNum || !arrDate || !branch) {
+            var error = "please ensure you have entered all required data";
+            $(".resultJSstring").hide().fadeIn(200).text(error);
+        } else {
+
+            var fullMsg = "you have selected our " + branch + " branch for " + guestNum + " " + peopleString + "," + " arriving on " + newDateWordMsg + ".";
+            $(".resultJSstring").hide().fadeIn(700).text(fullMsg);
+        }
+
+
+    });
+
+
+
 });
