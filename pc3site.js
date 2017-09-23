65d233fa75a43fdf5323f024244df0fd0aa1092f
sHale <sally.hale@homeport.org.uk>
2017-09-23 13:27:52 +0100

    sections revealed upon click

--- pc3site.js
+++ pc3site.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
 
     $("#aboutClick").on("click", function() {
-        $("#aboutClick").hide();
-
+        $("#about").removeClass("hidden").siblings().addClass('hidden');
+    });
+    $("#galleryClick").on("click", function() {
+        $("#gallery").removeClass("hidden").siblings().addClass('hidden');
+    });
+    $("#foodClick").on("click", function() {
+        $("#food").removeClass("hidden").siblings().addClass('hidden');
+    });
+    $("#eventsClick").on("click", function() {
+        $("#events").removeClass("hidden").siblings().addClass('hidden');
+    });
+    $("#contactClick").on("click", function() {
+        $("#contact").removeClass("hidden").siblings().addClass('hidden');
+    });
+    $("#reservationsClick").on("click", function() {
+        $("#reservations").removeClass("hidden").siblings().addClass('hidden');
     });
 });
